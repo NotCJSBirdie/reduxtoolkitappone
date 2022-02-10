1c108e10e8642eb4ce3728f33943882672ce6a5c
Carl Serquiña <serquinajhunlo@gmail.com>
2022-02-10 21:34:45 +0800

    withreactslick

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/index.tsx",
+    "./pages/_app.tsx",
+    "./pages/components/SlickCarousel.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
